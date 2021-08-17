3319e462122cc52d422102c083d15c9a91b03693
kareemadesola <kareemadesola1999@gmail.com>
2021-08-17 11:51:07 +0100

    implement with text change listener

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,31 +5,58 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
 import com.example.unitconverter.databinding.ActivityMainBinding
 const val  CUBIC_MILLIMETER_OUNCE = 29574
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val millimeterTextChangedListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            cubicMillimeterToOunce()
+        }
+
+    }
+    private val ounceTextChangedListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            ounceToCubicMillimeter()
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.millilitersValue.setOnClickListener { CubicMillimeterToOunce() }
-        binding.ouncesValue.setOnClickListener { OunceToCubicMillimeter() }
-
+        binding.millilitersValue.addTextChangedListener(millimeterTextChangedListener)
+        binding.ouncesValue.addTextChangedListener(ounceTextChangedListener)
     }
 
-    private fun CubicMillimeterToOunce() {
-        val valueInOunce = binding.millilitersValue.text.toString().toFloat().div(
+    private fun cubicMillimeterToOunce() {
+        binding.ouncesValue.removeTextChangedListener(ounceTextChangedListener)
+        val valueInOunce = binding.millilitersValue.text.toString().toFloatOrNull()?.div(
             CUBIC_MILLIMETER_OUNCE)
         binding.ouncesValue.setText("$valueInOunce", TextView.BufferType.EDITABLE)
+        binding.ouncesValue.addTextChangedListener(ounceTextChangedListener)
     }
 
-    private fun OunceToCubicMillimeter() {
-        val valueInMillimeter =  binding.ouncesValue.text.toString().toFloat().times(
+    private fun ounceToCubicMillimeter() {
+        binding.millilitersValue.removeTextChangedListener(millimeterTextChangedListener)
+        val valueInMillimeter = binding.ouncesValue.text.toString().toFloatOrNull()?.times(
             CUBIC_MILLIMETER_OUNCE)
         binding.millilitersValue.setText("$valueInMillimeter", TextView.BufferType.EDITABLE)
+        binding.millilitersValue.addTextChangedListener(millimeterTextChangedListener)
+
     }
 
 }
